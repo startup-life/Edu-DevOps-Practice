2f8879d44e73776ce7b10220ee84105d066d0400
RyeinKim <ryein6896@gmail.com>
2025-04-14 14:50:15 +0900

    fix: change user jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/ubuntu/backend
-docker compose pull
-docker compose up -d
+cd /var/lib/jenkins/backend
+/usr/local/bin/docker-compose pull
+/usr/local/bin/docker-compose up -d
