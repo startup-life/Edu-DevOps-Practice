22bc7eff09c22f17694d67bc82735dac0dbab668
RyeinKim <ryein6896@gmail.com>
2025-04-17 17:50:19 +0900

    feat: 줄바꿈 추가

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const VERSION = process.env.VERSION || 'unknown';
 
 app.get('/health', (req, res) => {
-    res.status(200).send(`OK - version: ${VERSION}`);
+    res.status(200).send(`OK - version: ${VERSION} \n`);
 });
 
 app.listen(PORT, () => {
