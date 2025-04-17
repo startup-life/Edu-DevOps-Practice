43402a95e19a70eacf8adaed885f516c9ef3acdb
RyeinKim <ryein6896@gmail.com>
2025-04-17 18:13:36 +0900

    fix: 헬스체크와 헬로월드 분기

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const app = express();
 const PORT = 3000;
 
 app.get('/health', (req, res) => {
+    res.status(200).send(`OK`);
+});
+
+app.get('/hello', (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     let count = 0;
 
     const interval = setInterval(() => {
-        res.write(`${count++}\n`);
-    }, 1000); // 1초마다 숫자 전송
+        res.write(`Hello World! - ${count++}\n`);
+    }, 1000);
 
     req.on('close', () => {
-        clearInterval(interval); // 클라이언트가 연결 종료하면 반복 중단
+        clearInterval(interval);
         console.log('Client disconnected');
     });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}, version: ${VERSION}`);
 });
