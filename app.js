adac008e63d3df7bcc23fcf33c5b841ca6c0efdc
RyeinKim <ryein6896@gmail.com>
2025-04-17 18:09:02 +0900

    fix: 응답 방식 변경

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const VERSION = process.env.VERSION || 'unknown';
 
 app.get('/health', (req, res) => {
-    res.status(200).send(`OK - version: ${VERSION} \n`);
+    res.setHeader('Content-Type', 'text/plain');
+    let count = 0;
+
+    const interval = setInterval(() => {
+        res.write(`${count++}\n`);
+    }, 1000); // 1초마다 숫자 전송
+
+    req.on('close', () => {
+        clearInterval(interval); // 클라이언트가 연결 종료하면 반복 중단
+        console.log('Client disconnected');
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}, version: ${VERSION}`);
+    console.log(`Server running on port ${PORT}`);
 });
