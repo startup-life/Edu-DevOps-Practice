951a8ee3e4ee8f0e9594bc3d4aa3b3fac7e6040f
RyeinKim <ryein6896@gmail.com>
2025-04-18 09:42:59 +0900

    fix: add datetime on healthcheck

--- app.js
+++ app.js
@@ -3,21 +3,13 @@ const app = express();
 const PORT = 3000;
 
 app.get('/health', (req, res) => {
-    res.status(200).send(`OK`);
-});
+    const now = new Date();
+    const hh = String(now.getHours()).padStart(2, '0');
+    const mm = String(now.getMinutes()).padStart(2, '0');
+    const ss = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hh}:${mm}:${ss}`;
 
-app.get('/hello', (req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    let count = 0;
-
-    const interval = setInterval(() => {
-        res.write(`Hello World! - ${count++}\n`);
-    }, 1000);
-
-    req.on('close', () => {
-        clearInterval(interval);
-        console.log('Client disconnected');
-    });
+    res.status(200).send(`OK - ${timeString}`);
 });
 
 app.listen(PORT, () => {
